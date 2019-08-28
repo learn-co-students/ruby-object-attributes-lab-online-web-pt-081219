4c7bbb26896342a2ef5c91237b8b2375fc849486
arnie b <arnold.bwaila@gmail.com>
2019-08-28 14:51:03 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  #def initialize(person_name, person_job)
+   #   @this_persons_name = person_name
+    #  @this_persons_job = person_job
+    #end
+  def name= (person_name)
+      @name = person_name
+    end
+  def name
+      @name
+    end
+  def job= (person_job)
+      @job = person_job
+    end
+  def job
+      @job
+    end
+  end
