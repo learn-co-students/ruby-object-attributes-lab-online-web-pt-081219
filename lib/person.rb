8809e40785bdcebf26b24033e99a20d5b3a1438c
Daniel Reina <dannyflatiron@gmail.com>
2019-09-05 23:44:03 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(current_job)
+    @job = current_job
+  end
+
+  def job
+    @job
+  end
+end
